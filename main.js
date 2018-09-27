42147d94748fb799ba0be824b90a351e6a4a7f18
GentillePlume <melvin.lemoine@feather-project.org>
2018-09-27 18:13:25 +0200

    Added keyboard controls

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 new WOW().init();
+
+var myAudio = document.getElementById('bgvid');
+myAudio.volume = 0;
+
+var volume = false;
+
+keyboardJS.bind(['enter'], function(e) {
+  if (volume == false) {
+    myAudio.volume = 1;
+    volume = true;
+  } else if (volume == true) {
+    myAudio.volume = 0;
+    volume = false;
+  } else {
+    alert("FATAL ERROR BURN DOWN SYSTEM AUTO DESTRUCTION");
+  }
+});
+
+var myVideo = document.getElementById('bgvid');
+var play = true;
+
+keyboardJS.bind(['ctrl'], function(e) {
+  if (play == false) {
+    myVideo.play();
+    play = true;
+  } else if (play == true) {
+    myVideo.pause();
+    play = false;
+  } else {
+    alert("FATAL ERROR BURN DOWN SYSTEM AUTO DESTRUCTION");
+  }
+});
